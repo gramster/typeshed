ffa978cdf2a7ffc19344fd3560664926076aa3da
Graham Wheeler <gram@geekraver.com>
2020-07-21 11:01:25 -0700

    Type stubs for pandas. These are not complete but are more extensive than any others we are aware of. They were written by the Python team at Microsoft for the pylance language server. Originally based off the ones at https://github.com/predictive-analytics-lab/data-science-types (snapped around December 2019).

---
+++ third_party/3/pandas/errors.pyi
@@ -0,0 +1,8 @@
+DtypeWarning: Exception
+EmptyDataError: Exception
+OutOfBoundsDatetime: Exception
+ParserError: Exception
+ParserWarning: Exception
+PerformanceWarning: Exception
+UnsortedIndexError: Exception
+UnsupportedFunctionCall: Exception
